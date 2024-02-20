f5fde25aa657136fe15f993df2cb9aeb68031789
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-02-19 19:45:40 -0500

    dotenv config and server setup

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Pull your server into this file and start it!
 */
+require('dotenv').config();
+const express = require('express')
+const cors = require('cors')
+const server = require('./api/server');
+
+const PORT = process.env.PORT || 9000;
+
+server.use(express.json());
+server.use(cors());
+
+
+server.get('*', (req, res) => {
+    res.json({
+        message: `API is working`
+    })
+})
+
+server.use((err, req, res, next) => {//eslint-disable-line 
+    res.status(500).json({
+        message: err.message, 
+        stack: err.stack,
+    })
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port :${PORT}`)
+})
